81cd98a9afa99cceb9a67512169a1f3f6423c37a
phongkeu2k3-star <phongkeu2k3@gmai.com>
2026-01-13 09:05:29 +0700

    Final-Tự động tạo db nếu không có

--- BaiTest_NguyenTienPhong_SQL/Program.cs
+++ BaiTest_NguyenTienPhong_SQL/Program.cs
@@ -1,12 +1,9 @@
-using BaiTest_NguyenTienPhong_SQL.Models;
+﻿using BaiTest_NguyenTienPhong_SQL.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -14,8 +11,21 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
+// tu dong sinh ra DB neu k co 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        context.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Có lỗi xảy ra khi tự động tạo Database.");
+    }
+}
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -23,13 +33,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.UseDefaultFiles();
-
 app.UseStaticFiles();
 
 app.Run();
